c93289ccf636b41c69fdba33a819c28eacf4816c
desar <ubuntu@DESKTOP-KA9NUBD.localdomain>
2022-05-05 10:08:36 +0900

    mod

--- keyboards/crkbd/keymaps/halberd/keymap.c
+++ keyboards/crkbd/keymaps/halberd/keymap.c
@@ -1,7 +1,4 @@
 /*
-<<<<<<< HEAD
-Copyright 2022 halberd
-=======
 Copyright 2019 @foostan
 Copyright 2020 Drashna Jaelre <@drashna>
 
@@ -17,38 +14,20 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
->>>>>>> 94663256ea791bb7671d61ef2210e7b03b6967aa
 */
 
 #include QMK_KEYBOARD_H
+#include <stdio.h>
 
-<<<<<<< HEAD
 #define DESKR LCTL(LWIN(KC_RGHT))
 #define DESKL LCTL(LWIN(KC_LEFT))
 
 #define JIME LALT(KC_GRV)
 #define SLEP MEH(KC_EQL)
 
-
 // Macro Ctrl-V + Enter
 enum custom_keycodes {
 	PSTEENT = SAFE_RANGE,
-};
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-	switch (keycode) {
-	case PSTEENT:
-		if (record->event.pressed) {
-			register_code(KC_LCTL);
-			register_code(KC_V);
-			unregister_code(KC_V);
-			unregister_code(KC_LCTL);
-			tap_code(KC_ENT);
-		} else {
-		}
-		break;
-	}
-	return true;
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -74,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
         KC_F6,   KC_F7,   KC_F9,   KC_F8,  KC_F10,  KC_F12,                      KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, KC_BSLS, KC_TILD,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                            MO(2),   TO(3),   TO(4),    XXXXXXX, XXXXXXX,   MO(1)
+                                            MO(2),   TO(3), XXXXXXX,    XXXXXXX, XXXXXXX,   MO(1)
                                       //`--------------------------'  `--------------------------'
   ),
   
@@ -83,27 +62,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
     A(KC_GRV),    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        DESKL, KC_HOME, XXXXXXX,   KC_UP, XXXXXXX, KC_PGUP,                      C(KC_C), C(KC_V), PSTEENT, XXXXXXX, XXXXXXX, XXXXXXX,
+        DESKL, KC_HOME, KC_BTN2,   KC_UP, KC_BTN1, KC_PGUP,                      XXXXXXX, C(KC_C), C(KC_V), PSTEENT, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
         DESKR,  KC_END, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                            MO(2), XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX,   MO(1)
+                                            MO(2), XXXXXXX, XXXXXXX,    XXXXXXX,   TO(4),   MO(1)
                                       //`--------------------------'  `--------------------------'
   ),
   
   // Left: RGB, Right: Mouse
   [3] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, XXXXXXX,                      KC_ACL0, KC_ACL1, KC_ACL2, XXXXXXX,   RESET,    SLEP,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX,   RESET,                      KC_WH_U, KC_BTN1, KC_MS_U, KC_BTN2, KC_WH_L, KC_BTN3,
+      RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX,                      KC_WH_U, KC_BTN1, KC_MS_U, KC_BTN2, KC_WH_L, KC_BTN3,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_MUTE, KC_VOLU, KC_VOLD, XXXXXXX, XXXXXXX,    SLEP,                      KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_R,  KC_APP,
+      KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX, KC_BTN2, KC_BTN1,                      KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_R,  KC_APP,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                            MO(2), XXXXXXX, XXXXXXX,    XXXXXXX,  XXXXXXX,   MO(1)
+                                            TO(0), XXXXXXX, XXXXXXX,    XXXXXXX,  XXXXXXX,   TO(0)
                                       //`--------------------------'  `--------------------------'
   ),
-
+									
   // Left: Audio, Right: NumPad
   [4] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
@@ -119,75 +98,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
+/*
 layer_state_t layer_state_set_user(layer_state_t state) {
     state = update_tri_layer_state(state, 1, 2, 3);
-=======
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
-enum crkbd_layers {
-    _QWERTY,
-    _LOWER,
-    _RAISE,
-    _ADJUST,
-};
-
-
-#define RAISE MO(_RAISE)
-#define LOWER MO(_LOWER)
-#define ADJUST MO(_ADJUST)
-#define DESKR LCTL(LWIN(KC_RGHT))
-#define DESKL LCTL(LWIN(KC_LEFT))
-
-#define COPY LCTL(KC_C)
-#define PSTE LCTL(KC_V)
-#define RERE LCTL(KC_R)
-
-#define JIME LALT(KC_GRV)
-#define SLEP MEH(KC_EQL)
-
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_QWERTY] = LAYOUT(
-    KC_TAB,  KC_Q,    KC_W,   KC_E,     KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ESC,
-    KC_LSFT, KC_A,    KC_S,   KC_D,     KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LCTL, KC_Z,    KC_X,   KC_C,     KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-                                        LOWER,   KC_BSPC, KC_LWIN, KC_LALT, KC_SPC,  RAISE
-  ),
-
-  [_LOWER] = LAYOUT(
-    JIME,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-    KC_BTN1, KC_BTN3, KC_LEFT, KC_UP,   KC_RGHT, DESKL,                     KC_WH_U, KC_BTN1, KC_MS_U, KC_BTN2, KC_WH_L, KC_BTN3,
-    KC_BTN2, RERE,    COPY,    KC_DOWN, PSTE,    DESKR,                     KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_R, XXXXXXX,
-                                        LOWER,   KC_BSPC, KC_LWIN, KC_LALT, KC_SPC,  RAISE
-  ),
-
-  [_RAISE] = LAYOUT(
-    JIME,    KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_APP,                    KC_MINS, KC_EQL,  KC_LCBR, KC_RCBR, KC_PIPE, KC_GRV,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, KC_BSLS, KC_TILD,
-                                        LOWER,   KC_BSPC, KC_LWIN, KC_LALT, KC_SPC,  RAISE
-
-  ),
-
-  [_ADJUST] = LAYOUT(
-    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-    RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, XXXXXXX,                   KC_INS,  KC_HOME, KC_PGUP, KC_PSCR, KC_SLCK, KC_PAUS,
-    RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX,                   KC_DEL,  KC_END,  KC_PGDN, XXXXXXX, RESET,   SLEP,
-                                        LOWER,   KC_BSPC, KC_LWIN, KC_LALT, KC_SPC,  RAISE
-  )
-};
-
-layer_state_t layer_state_set_user(layer_state_t state) {
-    state = update_tri_layer_state(state, _RAISE, _LOWER, _ADJUST);
->>>>>>> 94663256ea791bb7671d61ef2210e7b03b6967aa
     return state;
 }
+*/
 
 #ifdef OLED_DRIVER_ENABLE
-<<<<<<< HEAD
     #include "oled1.c"
-=======
-    #include "oled0.c"
->>>>>>> 94663256ea791bb7671d61ef2210e7b03b6967aa
 #endif
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+	switch (keycode) {
+	case PSTEENT:
+		if (record->event.pressed) {
+			register_code(KC_LCTL);
+			register_code(KC_V);
+			unregister_code(KC_V);
+			unregister_code(KC_LCTL);
+			tap_code(KC_ENT);
+		} else {
+		}
+		break;
+	}
+#ifdef OLED_DRIVER_ENABLE
+	return oled_process_record_user(keycode, record);
+#else
+	return true;
+#endif
+};
+
--- keyboards/crkbd/keymaps/halberd/rules.mk
+++ keyboards/crkbd/keymaps/halberd/rules.mk
@@ -1,13 +1,6 @@
-
-# Build Options
-#   change to "no" to disable the options, or define them in the Makefile in
-#   the appropriate keymap folder that will get included automatically
-#
-RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
-<<<<<<< HEAD
-MOUSEKEY_ENABLE  = yes
-EXTRAKEY_ENABLE  = yes
-#OLED_DRIVER_ENABLE  = yes
-=======
-OLED_DRIVER_ENABLE  = yes
->>>>>>> 94663256ea791bb7671d61ef2210e7b03b6967aa
+MOUSEKEY_ENABLE = yes    # Mouse keys
+RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
+EXTRAKEY_ENABLE = yes
+OLED_ENABLE     = yes
+# OLED_DRIVER_ENABLE  = no
+# OLED_DRIVER = SSD1306
